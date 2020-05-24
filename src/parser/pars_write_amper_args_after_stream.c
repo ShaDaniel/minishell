45625e1162a195806e78e5d28167711c7b830363
Egor <arara1196@gmail.com>
2020-05-24 20:50:58 +0300

    norminetting3

--- src/parser/pars_write_amper_args_after_stream.c
+++ src/parser/pars_write_amper_args_after_stream.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pars_write_amper_args_after_stream.c               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjonella <tjonella@student.42.fr>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 18:20:12 by tjonella          #+#    #+#             */
-/*   Updated: 2020/05/24 19:48:14 by tjonella           ###   ########.fr       */
+/*   Updated: 2020/05/24 20:44:51 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
